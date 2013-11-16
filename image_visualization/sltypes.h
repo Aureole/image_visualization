a82438afcef57f142a8feb900df797881e8bda7b
aureole <hfzhou1988@hotmail.com>
2013-11-16 16:04:16 +0800

    add method calculating heat image from gray image

---
+++ image_visualization/sltypes.h
@@ -0,0 +1,20 @@
+#ifndef IMAGE_VISUALIZATION_TYPES_H_ 
+#define IMAGE_VISUALIZATION_TYPES_H_ 
+
+typedef unsigned char  sl_uint8;
+typedef unsigned short sl_uint16;
+typedef unsigned  int  sl_uint32;
+typedef unsigned long  sl_uint64;
+
+typedef    char  sl_int8;  
+typedef __int16  sl_int16;
+typedef __int32  sl_int32;
+typedef __int64  sl_int64;
+
+#define sl_null  (void *)((sl_int32)0)
+
+typedef __int32  sl_bool;
+#define sl_true  (sl_bool)1
+#define sl_false (sl_bool)0
+
+#endif
---
+++ image_visualization/slvisualization.h
@@ -0,0 +1,9 @@
+#ifndef IMAGE_VISUALIZATION_VISUALIZATION_H_ 
+#define IMAGE_VISUALIZATION_VISUALIZATION_H_ 
+
+#include "cv.h"
+
+void slCalHeatImage(const IplImage *src, IplImage *dst);
+
+
+#endif
